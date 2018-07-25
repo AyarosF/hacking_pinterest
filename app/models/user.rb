7d2ea12f654f3ad5e375ad021f05f573ad1aaaa7
AyarosF <sorayafructuoso@gmail.com>
2018-07-25 22:24:32 +0200

    Ajout commentaires

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #un user peut créer plusieurs pins et plusieurs comments 
-	has_many :pins
-	has_many :comments
+  #un user peut créer plusieurs pins et plusieurs comments
+  has_many :pins
+  has_many :comments
 end
